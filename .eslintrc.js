5f305a83f85d9aaaabe842898c0f08725cafd81d
Anton Kolesnik <kolesnikanton@icloud.com>
2022-09-12 14:05:12 +0200

    Handle swipe

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
   ],
   rules: {
     'no-console': 'off',
+    'no-plusplus': 'off',
   },
 };
--- src/components/Swiper/index.jsx
+++ src/components/Swiper/index.jsx
@@ -1,69 +1,123 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/prop-types */
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
 
 import styles from './index.css';
 
 let isMoving = false;
 let startingClientX = 0;
 let distanceSwiped = 0;
-let slideIndex = 0;
 
 export default function Swiper({ children }) {
-  const SLIDE_WIDTH = 520;
+  const SLIDE_WIDTH = 500;
   const swiperRef = useRef(null);
-  const [translateX, setTranslateX] = useState(0);
+  const maxDistanceToSwipe = SLIDE_WIDTH * (children.length - 1);
 
   const handleStart = (event) => {
     isMoving = true;
-    startingClientX = event.nativeEvent.clientX;
+    startingClientX = event.nativeEvent.clientX + distanceSwiped;
   };
 
-  const handleEnd = () => {
-    if (isMoving) {
-      isMoving = false;
-      slideIndex += 1;
+  const getCurrentSlide = () => Math.round(distanceSwiped / SLIDE_WIDTH);
+
+  const roundSlide = () => {
+    const currentSlide = getCurrentSlide();
+    const currentSlideDistance = currentSlide * SLIDE_WIDTH;
+
+    while (currentSlideDistance !== distanceSwiped) {
+      if (currentSlideDistance > distanceSwiped) {
+        distanceSwiped += 1;
+      } else {
+        distanceSwiped -= 1;
+      }
+
+      swiperRef.current.style.transform = `translate(${-distanceSwiped}px, 0px)`;
     }
   };
 
+  const handleEnd = () => {
+    isMoving = false;
+    roundSlide();
+  };
+
   const handleSwipe = (event) => {
-    if (isMoving) {
-      const { clientX } = event.nativeEvent;
-      const distance = clientX - startingClientX;
+    if (!isMoving) {
+      return;
+    }
 
-      let translateValue = SLIDE_WIDTH * slideIndex;
+    const { clientX } = event.nativeEvent;
+    const distance = startingClientX - clientX;
 
-      distanceSwiped = distance;
+    if (distance < -50 || distance > maxDistanceToSwipe + 50) {
+      handleEnd();
+    }
 
-      console.log({ distanceSwiped, clientX, startingClientX });
+    swiperRef.current.style.transform = `translate(${-distance}px, 0px)`;
+    distanceSwiped = distance;
+  };
 
-      translateValue -= distanceSwiped;
+  const handleSwipeButton = ({ direction }) => {
+    let currentTranslateX = distanceSwiped + SLIDE_WIDTH;
 
-      swiperRef.current.style.transform = `translate(-${translateValue}px, 0px)`;
+    if (direction === 'left') {
+      currentTranslateX = distanceSwiped - SLIDE_WIDTH;
+    }
+
+    if (currentTranslateX > maxDistanceToSwipe || currentTranslateX < 0) {
+      return;
+    }
+
+    while (currentTranslateX !== distanceSwiped) {
+      if (currentTranslateX > distanceSwiped) {
+        distanceSwiped += 1;
+      } else {
+        distanceSwiped -= 1;
+      }
+
+      swiperRef.current.style.transform = `translate(${-distanceSwiped}px, 0px)`;
     }
   };
 
   const handleRightButton = () => {
-    const currentTranslateX = translateX - SLIDE_WIDTH;
-    setTranslateX(currentTranslateX);
-    swiperRef.current.style.transform = `translate(${currentTranslateX}px, 0px)`;
+    handleSwipeButton({ direction: 'right' });
   };
 
   const handleLeftButton = () => {
-    const currentTranslateX = translateX + SLIDE_WIDTH;
-    setTranslateX(currentTranslateX);
-    swiperRef.current.style.transform = `translate(${currentTranslateX}px, 0px)`;
+    handleSwipeButton({ direction: 'left' });
+  };
+
+  const handleTouchMove = (event) => {
+    const clientX = Math.round(event.touches[0].clientX);
+
+    const distance = startingClientX - clientX;
+
+    console.log(clientX, distance);
+
+    // if (distance < -50 || distance > maxDistanceToSwipe + 50) {
+    //   handleEnd();
+    // }
+
+    // swiperRef.current.style.transform = `translate(${-distance}px, 0px)`;
+    // distanceSwiped = distance;
   };
 
   return (
     <>
       <div className={styles.container}>
-        {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+        {/*
+          eslint-disable-next-line
+          jsx-a11y/no-static-element-interactions,
+          jsx-a11y/mouse-events-have-key-events
+        */}
         <div
           className={styles.swiper}
+          onMouseDown={handleStart}
           onMouseMove={handleSwipe}
           onMouseUp={handleEnd}
-          onMouseDown={handleStart}
+          onMouseOut={handleEnd}
+          // onTouchStart={handleStart}
+          // onTouchMove={handleTouchMove}
+          // onTouchEnd={handleEnd}
           ref={swiperRef}
         >
           {children.map((child, index) => (
