0e6fc4c557560012b623358b7bbd6e308ab6e6cd
Anton Kolesnik <kolesnikanton@icloud.com>
2023-05-06 20:39:21 +0200

    test

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,8 +10,6 @@ if [[ "$current_version" == "$last_commit_version" ]]; then
   echo "The version has not changed"
 else
   echo "The version has changed. Publishing"
-  echo "NODE_AUTH_TOKEN = $NODE_AUTH_TOKEN"
-  echo "//registry.npmjs.org/:_authToken=$NODE_AUTH_TOKEN" > ~/.npmrc
   npm whoami
   npm publish --dry-run --access public
 fi
